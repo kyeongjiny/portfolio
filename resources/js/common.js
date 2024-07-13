0fd0e42e1678f1872c39e601949c2bb6d2e7ec6a
JangKyungJin <jiny@pixdinewm.com>
2024-07-13 17:30:36 +0900

    포트폴리오 정리

--- resources/js/common.js
+++ resources/js/common.js
@@ -26,3 +26,54 @@ $(function () {
       })
 });
 
+/*tab*/
+function tab() {
+  var tab = $('[data-ui="tab"]');
+  tab.each(function(idx, item) {
+      var tabBtn = $(item).find(".tab-menu a"),
+          tabContent = $(item).find(".tab-cont-item");
+
+      tabBtn.find("span.hidden").remove();
+      tabBtn.each(function(idx, item) {
+          if($(item).parent("li").hasClass("is-open")) {
+              $(item).append('<span class="hidden">현재 페이지</span>');
+          } else {
+              $(item).append('<span class="hidden">해당 내용 보기</span>');
+          }
+      });
+      tabContent.each(function(idx, item) {
+          if($(item).hasClass("is-open")) {
+              $(item).attr("aria-hidden", false);
+          } else {
+              $(item).attr("aria-hidden", true);
+          }
+      });
+
+      tabBtn.on("click", function(e) {
+          e.preventDefault();
+          var target    = $(e.currentTarget),
+              targetLi  = target.parent("li"),
+              targetIdx = targetLi.index();
+
+          if(targetLi.hasClass("is-open")) {
+              return;
+          } else {
+              targetLi.addClass("is-open").siblings().removeClass("is-open");
+              $(tabContent[targetIdx]).addClass("is-open").attr({
+                  "aria-hidden" : false,
+                  "tabindex" : 0
+              }).siblings().removeClass("is-open").attr("aria-hidden", true).removeAttr("tabindex");
+          }
+      });
+  });
+}
+
+
+
+function init() {
+  tab();
+}
+
+$(function() {
+  init();
+});
